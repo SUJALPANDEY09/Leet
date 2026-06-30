0435a27cdd979c58289893d1e04b436f924065f1
SUJAL SANAT PANDEY <sujal69pandey@gmail.com>
2026-06-30 12:06:11 +0530

    Time: 1 ms (98.56%) | Memory: 47.4 MB (59.46%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum =0;
+        int r =0;
+        int l =0;
+        for(int i=0;i<nums.length;i++){
+            sum+= nums[i];
+        }
+
+        for(int i=0;i<nums.length;i++){
+            r = sum - nums[i];
+            sum -= nums[i];
+            if(r == l){
+                return i;
+            }
+            l += nums[i]; 
+        }
+        return -1;
+    }
+}
